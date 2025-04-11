d797888132e3c841bf41bc2c29032784b31204e7
Arthur Henryck <arthur.henryck@hotmail.com>
2025-04-11 12:22:27 -0300

    11-04-2025 pt2

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,7 +6,7 @@ function Cards({ produto, imagem }) {
         <>
             <div className="card7">{/*Card7*/}
                 <div className="bottom"></div>
-                <img src={imagem} className="limpezaimg" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ-nLrynCmahulc5wxe2t6v3DRZ4xw0HG49U_EdND2FrEWk_urGlvJ7BNmtMThDqVzSxI&usqp=CAU" className="limpezaimg" />
                 <p className="formatar texto"> {produto} </p>
             </div>
         </>
